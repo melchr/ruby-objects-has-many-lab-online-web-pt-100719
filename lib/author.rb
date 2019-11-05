3fe9f36ca78444fcaba78b0a8a089b938c858308
Melissa Christman <melchrstmn@gmail.com>
2019-11-05 22:48:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,13 +1,33 @@
 class Author
-  attr_accessor :name, :post
+  attr_accessor :name
+
+  @@authors = []
+
   def initialize(name)
     @name = name
+    @@authors << self
+  end
 
+  def add_post(post)
+    post.author = self
   end
-  def add_post(post=nil)
+
+   def self.authors
+    @@authors
   end
-  def add_post_by_title(title=nil)
+
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    add_post(new_post)
   end
-  def post_count
+
+  def posts
+    Post.all.select{|post|post.author == self}
+  end
+
+
+  def self.post_count
+   post_count = self.authors.collect{|author|author.posts}
+   post_count.flatten.count
   end
 end
